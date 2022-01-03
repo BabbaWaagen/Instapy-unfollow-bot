7a57e7591140dcb1157a8808f82a3756f31e3d87
BuddyDead <73744497+BuddyDead@users.noreply.github.com>
2022-01-03 09:13:33 +0100

    Update Unfollow Bot.py

--- Unfollow Bot.py
+++ Unfollow Bot.py
@@ -1,11 +1,15 @@
+#Import instapy
 from instapy import InstaPy
 #login
 session = InstaPy(username = '#InstagramUserName', password = '#InstagramPaswword', headless_browser = False)
 session.login()
 
 
-session.unfollow_users(amount = 366,
-                       allFollowing = True,
-                       unfollow_after = 0,
-                       sleep_delay = 600)
+session.unfollow_users(
+ amount = 366,
+ allFollowing = True,
+ unfollow_after = 0,
+ sleep_delay = 600
+)
+
 session.end()
